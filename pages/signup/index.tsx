50016be00a0aef9583e41b77eb38392d685006f7
Peter Yoo <trustintofu@gmail.com>
2022-12-10 22:47:19 -0800

    feat: Creates sign-up route.

---
+++ pages/signup/index.tsx
@@ -0,0 +1,13 @@
+import { SignUpForm } from '../../components/signup/SignUpForm.components';
+
+const SignUp = () => {
+  return (
+    <>
+      <section className="flex-grow h-full flex flex-col items-center justify-center">
+        <SignUpForm />
+      </section>
+    </>
+  );
+};
+
+export default SignUp;
