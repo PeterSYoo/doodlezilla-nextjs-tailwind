b3a55a021999d5980fb56a5aa92d6a324931711b
Peter Yoo <trustintofu@gmail.com>
2022-12-11 23:55:23 -0800

    feat: Adds linear gradient background.

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,20 +1,11 @@
-import { LoaderSpinner } from '../components/LoaderSpinner.components';
-
 export default function Home() {
   return (
     <>
-      {false ? (
-        <LoaderSpinner />
-      ) : (
-        <>
-          <div className="flex-grow h-full flex items-center justify-center">
-            Login
-            <form>
-              <label>Username</label>
-            </form>
-          </div>
-        </>
-      )}
+      <div className="flex-grow h-full flex items-center justify-center bg-gradient-to-tr from-[#5755D3] via-[#D2436C] to-[#F97E1C]">
+        <div className="bg-white w-full flex flex-col text-3xl">
+          <span>Text</span>
+        </div>
+      </div>
     </>
   );
 }
