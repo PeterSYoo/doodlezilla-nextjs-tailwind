67a13bd57605f0ed80f595f5dde15f52054a0ffc
Peter Yoo <trustintofu@gmail.com>
2022-12-11 23:54:32 -0800

    feat: Adds open sans to font family.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const { fontFamily } = require('tailwindcss/defaultTheme');
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: 'class',
@@ -10,6 +12,14 @@ module.exports = {
       animation: {
         spinFast: 'spin 0.35s linear infinite',
       },
+      fontFamily: {
+        primary: ['var(--openSans-font)', ...fontFamily.sans],
+        serif: ['var(--openSans-font)', ...fontFamily.serif],
+      },
+    },
+    screens: {
+      md: '769px',
+      lg: '1265px',
     },
   },
   plugins: [],
