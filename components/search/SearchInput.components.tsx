95ef89330c2ecf4d9ec5c1275a343393dc9e5f66
Peter Yoo <trustintofu@gmail.com>
2022-12-13 12:00:31 -0800

    feat: Creates search input.

---
+++ components/search/SearchInput.components.tsx
@@ -0,0 +1,25 @@
+import { useState } from 'react';
+import { MdSearch, MdClear } from 'react-icons/md';
+
+export const SearchInput = () => {
+  const [hasValue, setHasValue] = useState('');
+
+  return (
+    <>
+      <form>
+        <div className="bg-zinc-50 w-[188px] flex items-center gap-4 rounded-full border border-grayBorder px-4 h-[39px]">
+          {!hasValue && <MdSearch className="text-placeholder text-xl" />}
+          <input
+            placeholder="Search"
+            type="text"
+            onChange={(e) => setHasValue(e.target.value)}
+            className="bg-zinc-50 focus:outline-none w-full"
+          />
+          {hasValue && (
+            <MdClear className="text-xl text-placeholder cursor-pointer" />
+          )}
+        </div>
+      </form>
+    </>
+  );
+};
