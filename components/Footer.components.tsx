16455dcca5f8c8840c6b17d596e8028abab428b1
Peter Yoo <trustintofu@gmail.com>
2022-12-13 11:57:10 -0800

    feat: Creates footer navbar for mobile, highlights nav icons using router asPath, hides component for desktop.

--- components/Footer.components.tsx
+++ components/Footer.components.tsx
@@ -1,8 +1,37 @@
+import Image from 'next/image';
+import { useRouter } from 'next/router';
+import { AiFillHome } from 'react-icons/ai';
+import { CgAddR } from 'react-icons/cg';
+import { FiLogOut } from 'react-icons/fi';
+
 export const Footer = () => {
+  const router = useRouter();
+
+  const isFeedPage = router.asPath === '/feed';
+
   return (
     <>
-      <footer className="flex justify-center w-full pt-10 pb-20 bg-gray-300 dark:bg-gray-700">
-        <p className="text-sm">Coded and Designed by Peter Yoo. &#169; 2022</p>
+      <footer className="flex w-full h-[75px] bg-white dark:bg-gray-700 border-t border-grayBorder text-4xl px-9 pt-3">
+        <div className="flex justify-between w-full max-w-[375px] mx-auto">
+          {isFeedPage ? (
+            <div className="flex flex-col justify-between">
+              <AiFillHome className="text-sunset" />
+              <div className="w-[35px] h-[4px] bg-sunset rounded-t-2xl"></div>
+            </div>
+          ) : (
+            <AiFillHome />
+          )}
+          <CgAddR />
+          <div className="w-[35px] height-[35px]">
+            <Image
+              src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
+              alt="avatar 35px"
+              width={35}
+              height={35}
+            />
+          </div>
+          <FiLogOut />
+        </div>
       </footer>
     </>
   );
