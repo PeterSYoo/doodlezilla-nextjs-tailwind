28eee59b97ffa688a63daf75be0cbad7918666e5
Peter Yoo <trustintofu@gmail.com>
2022-12-13 19:40:04 -0800

    feat: Finished styling for mobile and desktop, adds more cards for testing.

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -5,9 +5,14 @@ import { FeedSponsoredCard } from '../../components/feed/FeedSponsoredCard.compo
 const FeedPage = () => {
   return (
     <>
-      <div className="md:ml-[94px] lg:ml-[213px] flex-grow flex flex-col justify-center items-center gap-5 mt-5 mb-24">
+      <div className="md:ml-[94px] md:mr-[159px] lg:ml-[213px] lg:mr-[258px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
+        <h1 className="hidden font-bold text-2xl md:flex justify-start md:w-[474px] lg:w-[733px] md:mt-5">
+          Feeds
+        </h1>
         <FeedPostCard />
         <FeedSponsoredCard />
+        <FeedPostCard />
+        <FeedPostCard />
       </div>
     </>
   );
