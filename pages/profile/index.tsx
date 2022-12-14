122cb5fb364e9e258ea299957c71c8ed8b819bd4
Peter Yoo <trustintofu@gmail.com>
2022-12-14 15:05:00 -0800

    feat: Creates profile route.

--- pages/profile/index.tsx
+++ pages/profile/index.tsx
@@ -1,7 +1,9 @@
 const ProfilePage = () => {
   return (
     <>
-      <div className="">Profile Page</div>
+      <div className="md:ml-[94px] lg:ml-[213px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
+        Profile Page
+      </div>
     </>
   );
 };
