94bc7505868c509d26c81d46904e9904a506d01d
Peter Yoo <trustintofu@gmail.com>
2022-12-13 19:40:38 -0800

    feat: Footer now holds all nav icons, properly styled for mobile.

--- components/Footer.components.tsx
+++ components/Footer.components.tsx
@@ -1,7 +1,6 @@
 import Image from 'next/image';
 import { useRouter } from 'next/router';
 import { AiFillHome } from 'react-icons/ai';
-import { CgAddR } from 'react-icons/cg';
 import { FiLogOut } from 'react-icons/fi';
 
 export const Footer = () => {
@@ -11,26 +10,34 @@ export const Footer = () => {
 
   return (
     <>
-      <footer className="flex w-full h-[75px] bg-white dark:bg-gray-700 border-t border-grayBorder text-4xl px-9 pt-3">
+      <footer className="z-40 fixed bottom-0 flex w-full h-[75px] bg-white dark:bg-gray-700 border-t border-grayBorder text-4xl px-9 pt-3 text-placeholder backdrop-blur-sm bg-opacity-75">
         <div className="flex justify-between w-full max-w-[375px] mx-auto">
           {isFeedPage ? (
             <div className="flex flex-col justify-between">
-              <AiFillHome className="text-sunset" />
+              <button>
+                <AiFillHome className="text-sunset" />
+              </button>
               <div className="w-[35px] h-[4px] bg-sunset rounded-t-2xl"></div>
             </div>
           ) : (
-            <AiFillHome />
+            <button>
+              <AiFillHome />
+            </button>
           )}
-          <CgAddR />
-          <div className="w-[35px] height-[35px]">
+          <button className="border-[3px] aspect-square h-[35px] flex justify-center items-center pb-1 text-3xl border-placeholder rounded-md">
+            +
+          </button>
+          <button className="w-[36px] height-[36px] flex">
             <Image
               src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
-              alt="avatar 35px"
-              width={35}
-              height={35}
+              alt="avatar 36px"
+              width={36}
+              height={36}
             />
-          </div>
-          <FiLogOut />
+          </button>
+          <button className="text-[35px] flex">
+            <FiLogOut />
+          </button>
         </div>
       </footer>
     </>
