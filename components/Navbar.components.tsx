a5f98f49627334c8492a490df0dc15ef3fe85dc1
Peter Yoo <trustintofu@gmail.com>
2022-12-14 11:32:36 -0800

    feat: Finshed styling for mobile and desktop. Adds highlighting of nav icons that are conditionally highlighted depending on the URL pathname that comes from useRouters asPath mathod.

--- components/Navbar.components.tsx
+++ components/Navbar.components.tsx
@@ -7,12 +7,15 @@ import { FiLogOut } from 'react-icons/fi';
 export const NavBar = () => {
   const router = useRouter();
 
+  const isProfilePage = router.asPath === '/profile';
   const isFeedPage = router.asPath === '/feed';
+  const isCreatePage = router.asPath === '/create';
 
   return (
     <>
       <div className="z-50 fixed bg-white h-full hidden md:flex md:w-[94px] lg:w-[213px] md:mr-[159px] lg:mr-[258px] border-r border-grayBorder md:flex-col">
         <div className="w-full h-[362px] border-b border-grayBorder flex flex-col justify-between">
+          {/* Logo for Mobile and Desktop */}
           <div className="w-full h-[41px] flex flex-col items-center justify-center mt-[19px]">
             <Link href="/feed">
               <Image
@@ -31,60 +34,140 @@ export const NavBar = () => {
               />
             </Link>
           </div>
+          {/*  */}
           <div className="flex flex-col justify-between items-center w-full">
-            <Image
-              src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
-              alt="avatar"
-              width={72}
-              height={72}
-            />
+            {/* Profile Avatar */}
+            {isProfilePage ? (
+              <div className="flex justify-start w-full pr-1 items-center">
+                <div className="w-[4px] h-[60px] lg:h-[72px] bg-sunset rounded-r-2xl"></div>
+                <div className="flex w-full justify-center">
+                  <Link href="/profile">
+                    <div className="animate-border rounded-full from-[#D055D3] via-sunset to-[#F97E1C] bg-[length:200%_200%] p-0.5 bg-black bg-gradient-to-tr duration-500">
+                      <div className="bg-white rounded-full p-0.5 flex justify-center items-center h-[60px] w-[60px] lg:h-[72px] lg:w-[72px]">
+                        <Image
+                          src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
+                          alt="avatar"
+                          width={72}
+                          height={72}
+                        />
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            ) : (
+              <div className="flex justify-start w-full pr-1 items-center">
+                <div className="w-[4px] h-[35px] bg-sunset rounded-r-2xl invisible"></div>
+                <div className="flex w-full justify-center">
+                  <Link href="/profile">
+                    <div className="hover:animate-border rounded-full hover:from-[#D055D3] hover:via-sunset hover:to-[#F97E1C] bg-[length:200%_200%] p-0.5 bg-white bg-gradient-to-tr duration-500">
+                      <div className="bg-white rounded-full p-0.5 flex justify-center items-center h-[60px] w-[60px] lg:h-[72px] lg:w-[72px]">
+                        <Image
+                          src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
+                          alt="avatar"
+                          width={72}
+                          height={72}
+                        />
+                      </div>
+                    </div>
+                  </Link>
+                </div>
+              </div>
+            )}
+            {/*  */}
+            {/* Username */}
             <p className="font-semibold mt-6 w-full break-words px-2 text-center">
               Apple
             </p>
+            {/*  */}
+            {/* Number of Doodles */}
             <div className="flex flex-col items-center my-9">
               <p className="text-xs font-bold">20</p>
-              <p className="text-xs text-placeholder">POSTS</p>
+              <p className="text-xs text-grayText">DOODLES</p>
             </div>
+            {/*  */}
           </div>
         </div>
         {/* Navigation */}
-        <div className="w-full border-b border-grayBorder flex flex-col items-center gap-11 text-placeholder pt-11 pb-11">
+        <div className="w-full border-b border-grayBorder flex flex-col items-center gap-11 text-grayText pt-11 pb-11">
+          {/* If Feed Page */}
           {isFeedPage ? (
-            <div className="flex justify-start w-full pr-1">
+            <div className="flex justify-start w-full pr-1 items-center">
               <div className="w-[4px] h-[35px] bg-sunset rounded-r-2xl"></div>
-              <button className="flex justify-center items-center text-xl text-sunset mx-auto lg:pr-10 lg:gap-5">
-                <AiFillHome />
-                <span className="text-[15px] font-bold hidden text-sunset lg:block">
-                  Feed
-                </span>
-              </button>
+              <Link
+                href="/feed"
+                className="flex justify-start mx-auto lg:ml-[45px]"
+              >
+                <div className="flex justify-center items-center">
+                  <button className="flex justify-center items-center text-xl text-sunset mx-auto lg:gap-5">
+                    <AiFillHome />
+                    <span className="text-[15px] font-bold hidden text-sunset lg:block">
+                      Feed
+                    </span>
+                  </button>
+                </div>
+              </Link>
             </div>
           ) : (
-            <div className="flex justify-start w-full pr-1">
+            <div className="flex justify-start w-full pr-1 items-center">
               <div className="w-[4px] h-[35px] bg-sunset rounded-r-2xl invisible"></div>
-              <button className="flex justify-center items-center text-xl mx-auto lg:pr-10 lg:gap-5">
-                <AiFillHome />
-                <span className="text-[15px] font-bold hidden lg:block">
-                  Feed
-                </span>
-              </button>
+              <Link
+                href="/feed"
+                className="flex justify-start mx-auto lg:ml-[45px] group"
+              >
+                <div className="flex justify-center items-center">
+                  <button className="flex justify-center items-center text-xl text-grayText group-hover:text-sunset mx-auto lg:gap-5">
+                    <AiFillHome />
+                    <span className="text-[15px] font-bold hidden text-grayText group-hover:text-sunset lg:block">
+                      Feed
+                    </span>
+                  </button>
+                </div>
+              </Link>
             </div>
           )}
-          <div className="flex justify-start w-full pr-1">
-            <div className="w-[4px] h-[35px] bg-sunset rounded-t-2xl invisible"></div>
-            <div className="lg:flex lg:items-center mx-auto lg:gap-5 lg:pr-6">
-              <button className="border-[2px] aspect-square h-5 w-5 flex justify-center items-center text-lg border-placeholder rounded-md pl-[1px] lg:pl-0 lg:pr-[1px] pb-[2px] mx-auto">
-                +
-              </button>
-              <span className="text-[15px] font-bold hidden lg:block">
-                Create
-              </span>
+          {/*  */}
+          {/* If Create Page */}
+          {isCreatePage ? (
+            <div className="flex justify-start w-full pr-1 items-center">
+              <div className="w-[4px] h-[35px] bg-sunset rounded-r-2xl"></div>
+              <div className="lg:flex lg:items-center mx-auto lg:gap-5 lg:pr-6 text-sunset">
+                <Link
+                  href="/create"
+                  className="flex justify-start items-center mx-auto group lg:gap-5"
+                >
+                  <button className="border-[2px] aspect-square h-5 w-5 flex justify-center items-center text-lg border-sunset rounded-md pl-[1px] lg:pl-0 lg:pr-[1px] pb-[2px] mx-auto">
+                    +
+                  </button>
+                  <span className="text-[15px] font-bold hidden lg:block">
+                    Create
+                  </span>
+                </Link>
+              </div>
             </div>
-          </div>
+          ) : (
+            <div className="flex justify-start w-full pr-1 items-center">
+              <div className="w-[4px] h-[35px] bg-sunset rounded-r-2xl invisible"></div>
+              <div className="lg:flex lg:items-center mx-auto lg:gap-5 lg:pr-6">
+                <Link
+                  href="/create"
+                  className="flex justify-start items-center mx-auto group lg:gap-5"
+                >
+                  <button className="border-[2px] aspect-square h-5 w-5 flex justify-center items-center text-lg border-grayText group-hover:border-sunset group-hover:text-sunset rounded-md pl-[1px] lg:pl-0 lg:pr-[1px] pb-[2px] mx-auto">
+                    +
+                  </button>
+                  <span className="text-[15px] font-bold hidden lg:block group-hover:text-sunset">
+                    Create
+                  </span>
+                </Link>
+              </div>
+            </div>
+          )}
+          {/*  */}
         </div>
         {/*  */}
         <div className="w-full">
-          <button className="text-[22px] flex items-center justify-center mt-11 w-full text-placeholder lg:gap-5 lg:pr-6">
+          <button className="text-[22px] flex items-center justify-center mt-11 ml-[37px] lg:ml-[48px] text-grayText hover:text-sunset lg:gap-5">
             <FiLogOut />
             <span className="text-[15px] font-bold hidden lg:block">
               Logout
