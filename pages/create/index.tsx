bc61a166d464b321503b199323e97eae3f9c051e
Peter Yoo <trustintofu@gmail.com>
2022-12-14 15:04:52 -0800

    feat: Creates create page.

---
+++ pages/create/index.tsx
@@ -0,0 +1,13 @@
+const CreatePage = () => {
+  return (
+    <>
+      <div className="md:ml-[94px] lg:ml-[213px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
+        <div className="h-[2000px] flex justify-center items-center">
+          Create Page
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default CreatePage;
