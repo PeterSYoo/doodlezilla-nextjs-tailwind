ae138e1f2afe79a19a12c00667dafa0389eeb19f
Peter Yoo <trustintofu@gmail.com>
2022-12-13 19:41:08 -0800

    refactor: Moves components.

--- components/RightBar.components.tsx
+++
@@ -1,9 +0,0 @@
-export const RightBar = () => {
-  return (
-    <>
-      <div className="fixed bg-purple-300 right-0 h-full hidden md:flex md:w-[159px] lg:w-[258px]">
-        RightBar2
-      </div>
-    </>
-  );
-};
