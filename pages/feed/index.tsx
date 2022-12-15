c606ad91623ef034572bab09b5fbf9298689d17f
Peter Yoo <trustintofu@gmail.com>
2022-12-14 19:07:12 -0800

    refactor: Changes header title.

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -7,7 +7,7 @@ const FeedPage = () => {
     <>
       <div className="md:ml-[94px] md:mr-[159px] lg:ml-[213px] lg:mr-[258px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
         <h1 className="hidden font-bold text-2xl md:flex justify-start md:w-[474px] lg:w-[733px] md:mt-5">
-          Feeds
+          Feed
         </h1>
         <FeedDoodleCard />
         <FeedSponsoredCard />
