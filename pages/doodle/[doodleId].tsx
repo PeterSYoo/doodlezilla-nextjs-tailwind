42fe0ed69a9509db6acedd65c4d26592885c9ad5
Peter Yoo <trustintofu@gmail.com>
2022-12-14 19:06:43 -0800

    feat: Creates slug page for doodles.

---
+++ pages/doodle/[doodleId].tsx
@@ -0,0 +1,13 @@
+const DoodleIdPage = () => {
+  return (
+    <>
+      <div className="md:ml-[94px] lg:ml-[213px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
+        <div className="flex justify-center items-center bg-red-300">
+          Doodle Page
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default DoodleIdPage;
