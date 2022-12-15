a92068e7d7948a5a72966d660a918bd0ac48473c
Peter Yoo <trustintofu@gmail.com>
2022-12-14 19:05:27 -0800

    refactor: Adds semantic comments.

--- components/login/LoginForm.components.tsx
+++ components/login/LoginForm.components.tsx
@@ -5,6 +5,7 @@ export const LoginForm = () => {
     <>
       <form>
         <div className="flex flex-col gap-3">
+          {/* Username */}
           <label className="w-[278px] bg-grayLight py-2 px-4 rounded-full">
             <input
               placeholder="Username"
@@ -12,6 +13,8 @@ export const LoginForm = () => {
               className="w-full bg-grayLight focus:outline-none"
             />
           </label>
+          {/*  */}
+          {/* Password */}
           <label className="w-[278px] bg-grayLight py-2 pl-4 rounded-full flex items-center">
             <input
               placeholder="Password"
@@ -20,12 +23,15 @@ export const LoginForm = () => {
             />
             <FaRegEye className="w-2/12 flex justify-end pr-2 text-xl text-placeholder cursor-pointer hover:text-neutral-800" />
           </label>
+          {/*  */}
+          {/* Login Button */}
           <button
             type="submit"
             className="py-2 px-10 flex items-center justify-center gap-3 rounded-full bg-gradient-to-t from-[#5755D3] to-cobalt w-[278px] text-white font-semibold transition duration-300 ease-in-out hover:animate-button hover:bg-[length:400%_400%] hover:from-[#F97E1C] hover:via-sunset hover:to-[#5755D3]"
           >
             Login
           </button>
+          {/*  */}
         </div>
       </form>
     </>
