975a5467c0066ca54b08976c3f3c797699285e7e
Peter Yoo <trustintofu@gmail.com>
2022-12-15 23:08:47 -0800

    feat: Creates doodle slug route.

--- pages/doodle/[doodleId].tsx
+++ pages/doodle/[doodleId].tsx
@@ -1,10 +1,10 @@
+import { DoodleCard } from '../../components/doodle/DoodleCard.components';
+
 const DoodleIdPage = () => {
   return (
     <>
-      <div className="md:ml-[94px] lg:ml-[213px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
-        <div className="flex justify-center items-center bg-red-300">
-          Doodle Page
-        </div>
+      <div className="md:ml-[94px] lg:ml-[213px] flex-grow flex flex-col items-center gap-5 mt-24 mb-32 md:justify-start">
+        <DoodleCard />
       </div>
     </>
   );
