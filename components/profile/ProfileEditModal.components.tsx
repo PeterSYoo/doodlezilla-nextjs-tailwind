aae627b3c7bf745f172543710a5d46e4d8987532
Peter Yoo <trustintofu@gmail.com>
2022-12-17 21:10:07 -0800

    fix: Adds type definitions.

--- components/profile/ProfileEditModal.components.tsx
+++ components/profile/ProfileEditModal.components.tsx
@@ -1,6 +1,10 @@
 import { RiCloseFill } from 'react-icons/ri';
 
-export const ProfileEditModal = ({ setIsModal }: any) => {
+export interface DoodleCardModalProps {
+  setIsModal: (isModal: boolean) => void;
+}
+
+export const ProfileEditModal = ({ setIsModal }: DoodleCardModalProps) => {
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-screen z-50 bg-black bg-opacity-60 flex justify-center items-center">
