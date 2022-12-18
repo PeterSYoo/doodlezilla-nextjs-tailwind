bcbbd4426b454002cb66b0edc3b3f2b1871273f3
Peter Yoo <trustintofu@gmail.com>
2022-12-17 16:16:04 -0800

    fix: Adds type definitions.

--- database/usersConnect.ts
+++ database/usersConnect.ts
@@ -1,13 +1,15 @@
 import mongoose from 'mongoose';
 
-const DB_URL: any = process.env.NEXT_PUBLIC_MONGODB_URL_USERDB;
+const DB_URL: string = process.env.NEXT_PUBLIC_MONGODB_URL_USERDB as string;
 
-const usersConnect = async () => {
+const usersConnect = async (): Promise<boolean> => {
   try {
     const { connection } = await mongoose.connect(DB_URL);
 
-    if (connection.readyState == 1) {
+    if (connection.readyState === 1) {
       return Promise.resolve(true);
+    } else {
+      return Promise.resolve(false);
     }
   } catch (error) {
     return Promise.reject(error);
