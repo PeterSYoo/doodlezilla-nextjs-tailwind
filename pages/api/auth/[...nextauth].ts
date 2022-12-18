047ed4c3bb3f2fb8da684a16ffd7ae6999b5f0ae
Peter Yoo <trustintofu@gmail.com>
2022-12-17 21:07:16 -0800

    feat: Adds google provider and mongodb adapter.

--- pages/api/auth/[...nextauth].ts
+++ pages/api/auth/[...nextauth].ts
@@ -3,38 +3,62 @@ import CredentialsProvider from 'next-auth/providers/credentials';
 import usersConnect from '../../../database/usersConnect';
 import Users from '../../../models/Users';
 import { compare } from 'bcrypt';
+import GoogleProvider from 'next-auth/providers/google';
+import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
+import clientPromise from '../../../database/clientPromise';
 
 export const authOptions: any = {
   providers: [
-    // @ts-ignore
+    GoogleProvider({
+      clientId: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.NEXT_PUBLIC_GOOGLE_CLIENT_SECRET as string,
+    }),
     CredentialsProvider({
       name: 'Credentials',
+      /* Fixed Type Error Adding this Block */
+      credentials: {
+        username: {
+          label: 'Username',
+          type: 'text',
+          required: true,
+        },
+        password: {
+          label: 'Password',
+          type: 'password',
+          required: true,
+        },
+      },
+      /*  */
       async authorize(credentials: any, req) {
         usersConnect().catch((error) => {
           error: 'Connection Failed...!';
         });
 
-        // check user existance
+        /* Check User Existance */
         const result = await Users.findOne({ username: credentials?.username });
         if (!result) {
           throw new Error('No user found with that Username!');
         }
+        /*  */
 
-        // compare
+        /* Compare Passwords */
         const checkPassword = await compare(
           credentials?.password,
           result.password
         );
+        /*  */
 
-        // incorrect password
+        /* Incorrect Password */
         if (!checkPassword) {
           throw new Error('Wrong password!');
         }
+        /*  */
 
         return result;
       },
     }),
   ],
+  adapter: MongoDBAdapter(clientPromise),
   callbacks: {
     async signIn({ user, account, profile, email, credentials }: any) {
       return true;
