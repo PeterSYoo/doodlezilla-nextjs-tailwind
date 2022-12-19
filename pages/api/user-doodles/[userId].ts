41dff98456fadfebaa4e13aa7109239a3ed7a55b
Peter Yoo <trustintofu@gmail.com>
2022-12-18 16:13:41 -0800

    feat: Creates slug api endpoint for user's doodles.

---
+++ pages/api/user-doodles/[userId].ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getUserDoodles } from '../../../controllers/doodlesController';
+import usersConnect from '../../../database/usersConnect';
+
+export default async function usersIdApi(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  usersConnect().catch(() => {
+    res.status(405).json({ error: 'Error in connecting to users database' });
+  });
+
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      getUserDoodles(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
