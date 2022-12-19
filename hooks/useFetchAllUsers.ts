0d3e2203778c985615adaac697dc3e37e86ac5c9
Peter Yoo <trustintofu@gmail.com>
2022-12-18 19:04:13 -0800

    feat: Creates custom hook that fetches all users.

---
+++ hooks/useFetchAllUsers.ts
@@ -0,0 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
+
+const useFetchAllUsers = () => {
+  const {
+    data: allUsersData,
+    isLoading: allUsersIsLoading,
+    isError: allUsersIsError,
+  } = useQuery(['allUsers'], async () => {
+    try {
+      const response = await fetch(`/api/users`);
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  return { allUsersData, allUsersIsLoading, allUsersIsError };
+};
+
+export default useFetchAllUsers;
