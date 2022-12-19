9b3809c9355f2d23b36b14e4765c4e2c6935b4b1
Peter Yoo <trustintofu@gmail.com>
2022-12-18 19:11:37 -0800

    feat: Implements custom hooks that fetches user by username and doodles by username. If user is not found then show not found message on JSX.

--- components/profile/ProfileOtherUsersRightBar.components.tsx
+++ components/profile/ProfileOtherUsersRightBar.components.tsx
@@ -1,62 +1,101 @@
 import Image from 'next/image';
+import useFetchUserDoodles from '../../hooks/useFetchUsersDoodles';
+import useFetchUserByUsername from '../../hooks/useFetchUserByUsername';
+import { LoaderSpinner } from '../LoaderSpinner.components';
+import useFetchUserDoodlesByUsername from '../../hooks/useFetchUsersDoodlesByUsername';
 
-export const ProfileOtherUsersRightBar = () => {
+export const ProfileOtherUsersRightBar = ({ session, username }: any) => {
+  const { userData, userIsLoading, userIsError } =
+    useFetchUserByUsername(username);
+  const { userDoodlesData, userDoodlesIsLoading, userDoodlesIsError } =
+    useFetchUserDoodlesByUsername(username);
+
+  if (userIsLoading || userDoodlesIsLoading) return <LoaderSpinner />;
+  if (userIsError) return <>Error</>;
+
   return (
     <>
-      <div className="z-40 fixed bg-white right-0 h-full hidden md:flex md:w-[159px] lg:w-[258px] border-l border-grayBorder md:flex-col px-1">
-        <div className="md:flex md:flex-col lg:w-[187px] md:mx-auto md:gap-6">
-          <div className="flex justify-between items-center mt-[18px]">
-            <h1 className="font-semibold ml-2 lg:ml-0">Profile</h1>
-          </div>
-          {/* Profile Avatar */}
-          <div className="flex justify-center">
-            <Image
-              src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1671059751/nudoodle/assets/download_2_qkqj5l.png"
-              width={140}
-              height={140}
-              alt="profile avatar"
-              className="lg:hidden"
-            />
-            <Image
-              src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1671059751/nudoodle/assets/download_2_qkqj5l.png"
-              width={200}
-              height={200}
-              alt="profile avatar"
-              className="md:hidden lg:block"
-            />
-          </div>
-          {/*  */}
-          <div className="flex flex-col md:px-2 lg:px-0 gap-1 md:items-center lg:items-start">
-            {/* Username */}
-            <h1 className="font-bold w-full break-all text-lg">APPLE</h1>
+      {userData._id ? (
+        <div className="z-40 fixed bg-white right-0 h-full hidden md:flex md:w-[159px] lg:w-[258px] border-l border-grayBorder md:flex-col px-1">
+          <div className="md:flex md:flex-col lg:w-[187px] md:mx-auto md:gap-6">
+            <div className="flex justify-between items-center mt-[18px]">
+              <h1 className="font-semibold ml-2 lg:ml-0">Profile</h1>
+            </div>
+            {/* Profile Avatar */}
+            <div className="flex justify-center">
+              <Image
+                src={
+                  userData.image
+                    ? userData.image
+                    : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
+                }
+                width={140}
+                height={140}
+                alt="profile avatar"
+                className="lg:hidden rounded-full"
+              />
+              <Image
+                src={
+                  userData.image
+                    ? userData.image
+                    : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
+                }
+                width={200}
+                height={200}
+                alt="profile avatar"
+                className="md:hidden lg:block rounded-full"
+              />
+            </div>
             {/*  */}
-            <div className="flex flex-col justify-between lg:items-start gap-6">
-              {/* Doodles Count */}
-              <p className="font-semibold text-xs">
-                20 <span className="text-placeholder">Doodles</span>
-              </p>
+            <div className="flex flex-col md:px-2 lg:px-0 gap-1 md:items-center lg:items-start">
+              {/* Username */}
+              <h1 className="font-bold w-full break-all text-lg">
+                {userData.name.toUpperCase()}
+              </h1>
               {/*  */}
-              {/* Bio */}
-              <p className="text-xs text-placeholder">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-                Facilis, tenetur earum id ducimus ipsam fuga error sit odio in
-                harum voluptatibus debitis cumque maiores unde vitae a! Veniam,
-                voluptatum nam?
-              </p>
-              <p className="text-xs text-placeholder">
-                Lorem ipsum dolor sit, amet consectetur adipisicing elit.
-              </p>
-              {/*  */}
-              {/* Location */}
-              <div className="flex flex-col lg:px-0 gap-1 lg:items-start">
-                <p className="font-semibold text-xs">Location</p>
-                <p className="text-xs text-placeholder">Based in Japan.</p>
+              <div className="flex flex-col justify-between lg:items-start gap-6">
+                {/* Doodles Count */}
+                <p className="font-semibold text-xs">
+                  {userDoodlesData.length}&nbsp;
+                  <span className="text-placeholder">Doodles</span>
+                </p>
+                {/*  */}
+                {/* Bio */}
+                <p className="text-xs text-placeholder">
+                  {userData.biography ? (
+                    userData.biography
+                  ) : (
+                    <>This user hasn&apos;t written a biography yet.</>
+                  )}
+                </p>
+                {/*  */}
+                {/* Location */}
+                <div className="flex flex-col lg:px-0 gap-1 lg:items-start">
+                  <p className="font-semibold text-xs">Location</p>
+                  <p className="text-xs text-placeholder">
+                    {userData.location ? (
+                      userData.location
+                    ) : (
+                      <>This user hasn&apos;t added their location yet.</>
+                    )}
+                  </p>
+                </div>
+                {/*  */}
               </div>
-              {/*  */}
             </div>
           </div>
         </div>
-      </div>
+      ) : (
+        <>
+          <div className="z-40 fixed bg-white right-0 h-full hidden md:flex md:w-[159px] lg:w-[258px] border-l border-grayBorder md:flex-col px-1">
+            <div className="md:flex md:flex-col lg:w-[187px] md:mx-auto md:gap-6 h-full">
+              <div className="flex justify-center items-center h-full">
+                <h1 className="font-semibold">No User Found</h1>
+              </div>
+            </div>
+          </div>
+        </>
+      )}
     </>
   );
 };
