8543d65c3eb79262be99318937265f2d74829bd6
Peter Yoo <trustintofu@gmail.com>
2022-12-20 01:48:15 -0800

    feat: Implements useMutation hook to call API to delete doodle on delete button click.

--- components/profile/doodle/ProfileDoodleOptionsModal.components.tsx
+++ components/profile/doodle/ProfileDoodleOptionsModal.components.tsx
@@ -1,12 +1,35 @@
+import { useMutation } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
 import { RiCloseFill } from 'react-icons/ri';
 
 export interface DoodleCardModalProps {
   setIsModal: (isModal: boolean) => void;
+  doodleWithCommentsData: any;
 }
 
 export const ProfileDoodleOptionsModal = ({
+  doodleWithCommentsData,
   setIsModal,
 }: DoodleCardModalProps) => {
+  const router = useRouter();
+
+  const { mutate, isLoading } = useMutation(async (doodleId) => {
+    try {
+      const Options = {
+        method: 'DELETE',
+        headers: { 'Content-Type': 'application/json' },
+      };
+      const response = await fetch(`/api/doodles/${doodleId}`, Options);
+      const json = await response.json();
+
+      if (json) {
+        router.push('/profile');
+      }
+    } catch (error) {
+      return error;
+    }
+  });
+
   return (
     <>
       <div className="fixed top-0 left-0 w-full h-screen z-50 bg-black bg-opacity-60 flex justify-center items-center">
@@ -18,7 +41,10 @@ export const ProfileDoodleOptionsModal = ({
         </button>
         <div className="container mx-auto w-11/12 md:w-96">
           <div className="relative py-6 bg-white rounded-3xl flex flex-col gap-6 items-center">
-            <button className="font-semibold text-sunset hover:text-neutral-800">
+            <button
+              onClick={() => mutate(doodleWithCommentsData.doodle._id)}
+              className="font-semibold text-sunset hover:text-neutral-800"
+            >
               Delete
             </button>
             <div className="border-b border-grayBorder w-11/12"></div>
