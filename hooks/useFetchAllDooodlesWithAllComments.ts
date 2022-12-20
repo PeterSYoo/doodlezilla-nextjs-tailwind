660db303ffee49ae5e806d311ed22079f9b758fd
Peter Yoo <trustintofu@gmail.com>
2022-12-19 22:27:08 -0800

    feat: Creates custom hook that fetches all doodles with all comments.

---
+++ hooks/useFetchAllDooodlesWithAllComments.ts
@@ -0,0 +1,26 @@
+import { useQuery } from '@tanstack/react-query';
+
+const useFetchAllDoodlesWithAllComments = () => {
+  const {
+    data: allDoodlesWithCommentsData,
+    isLoading: allDoodlesWithCommentsIsLoading,
+    isError: allDoodlesWithCommentsIsError,
+    refetch: allDoodlesWithCommentsRefetch,
+  } = useQuery(['allDoodlesWithComments'], async () => {
+    try {
+      const response = await fetch(`/api/doodle-with-all-comments/`);
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  return {
+    allDoodlesWithCommentsData,
+    allDoodlesWithCommentsIsLoading,
+    allDoodlesWithCommentsIsError,
+    allDoodlesWithCommentsRefetch,
+  };
+};
+
+export default useFetchAllDoodlesWithAllComments;
