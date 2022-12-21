37fc735e69a134fbd927465ebbf09c99681b1eb9
Peter Yoo <trustintofu@gmail.com>
2022-12-20 21:37:09 -0800

    feat: Creates API route for user-is-likes-doodle.

---
+++ pages/api/user-is-likes-doodle/[userId]/index.ts
@@ -0,0 +1,24 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import { getUserIsLikesDoodle } from '../../../../controllers/isLikesController';
+
+import usersConnect from '../../../../database/usersConnect';
+export default async function usersApi(
+  req: NextApiRequest,
+  res: NextApiResponse
+) {
+  usersConnect().catch(() => {
+    res.status(405).json({ error: 'Error in connecting to users database' });
+  });
+
+  const { method } = req;
+
+  switch (method) {
+    case 'GET':
+      getUserIsLikesDoodle(req, res);
+      break;
+    default:
+      res.setHeader('Allow', ['GET']);
+      res.status(405).end(`Method ${method} Not Allowed`);
+      break;
+  }
+}
