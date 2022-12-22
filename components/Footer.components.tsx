02abbaf20ea0616ad243f2d2e7de222d0d98f9d1
Peter Yoo <trustintofu@gmail.com>
2022-12-21 21:59:52 -0800

    feat: Updates profile image, changes margins on create for mobile.

--- components/Footer.components.tsx
+++ components/Footer.components.tsx
@@ -1,21 +1,33 @@
-import { signOut } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import Image from 'next/image';
 import Link from 'next/link';
 import { useRouter } from 'next/router';
 import { AiFillHome } from 'react-icons/ai';
 import { FiLogOut } from 'react-icons/fi';
+import useFetchUser from '../hooks/useFetchUser';
+import { LoaderSpinner } from './LoaderSpinner.components';
 
 export const Footer = () => {
   const router = useRouter();
+
+  const { data: session }: any = useSession();
+
+  const { userData, userIsLoading, userIsError, userRefetch } = useFetchUser(
+    session?.user?.id
+  );
 
   const isProfilePage = router.asPath === '/profile';
   const isFeedPage = router.asPath === '/feed';
   const isCreatePage = router.asPath === '/create';
 
+  if (userIsLoading) return <LoaderSpinner />;
+  if (userIsError) return <>Error</>;
+
   return (
     <>
       <footer className="z-40 fixed bottom-0 flex w-full h-[75px] bg-white dark:bg-gray-700 border-t border-grayBorder text-4xl px-9 pt-3 text-placeholder backdrop-blur-sm bg-opacity-75">
         <div className="flex justify-between w-full max-w-[375px] mx-auto">
+          {/* Feed Button */}
           {isFeedPage ? (
             <div className="flex flex-col justify-between">
               <Link href="/feed">
@@ -35,10 +47,12 @@ export const Footer = () => {
               <div className="w-[35px] h-[4px] bg-sunset rounded-t-2xl invisible"></div>
             </div>
           )}
+          {/*  */}
+          {/* Create Button */}
           {isCreatePage ? (
             <div className="flex flex-col justify-between">
               <Link href="/create">
-                <button className="border-[3px] aspect-square h-[35px] flex justify-center items-center pb-1 text-3xl border-sunset text-sunset rounded-md">
+                <button className="border-[3px] aspect-square h-[35px] flex justify-center items-center text-3xl border-sunset text-sunset rounded-md">
                   +
                 </button>
               </Link>
@@ -46,21 +60,28 @@ export const Footer = () => {
             </div>
           ) : (
             <Link href="/create">
-              <button className="border-[3px] aspect-square h-[35px] flex justify-center items-center pb-1 text-3xl border-placeholder rounded-md">
+              <button className="border-[3px] aspect-square h-[35px] flex justify-center items-center text-3xl border-placeholder rounded-md">
                 +
               </button>
             </Link>
           )}
+          {/*  */}
+          {/* Profile Button */}
           {isProfilePage ? (
             <div className="-mt-1 flex flex-col justify-between items-center">
               <Link href="/profile">
                 <div className="animate-border rounded-full from-[#D055D3] via-sunset to-[#F97E1C] bg-[length:400%_400%] p-0.5 bg-black bg-gradient-to-tr duration-300">
                   <button className="bg-white rounded-full p-0.5 flex justify-center items-center">
                     <Image
-                      src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
+                      src={
+                        userData.image
+                          ? userData.image
+                          : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
+                      }
                       alt="avatar 36px"
                       width={36}
                       height={36}
+                      className="rounded-full"
                     />
                   </button>
                 </div>
@@ -73,10 +94,15 @@ export const Footer = () => {
                 <div className="animate-border rounded-full from-transparent via-transparent to-transparent bg-[length:400%_400%] p-0.5 bg-transparent bg-gradient-to-tr duration-300">
                   <button className="bg-transparent rounded-full p-0.5 flex justify-center items-center">
                     <Image
-                      src="https://res.cloudinary.com/dryh1nvhk/image/upload/v1670910840/nudoodle/assets/cat_avatar_cmp6xf.png"
+                      src={
+                        userData.image
+                          ? userData.image
+                          : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
+                      }
                       alt="avatar 36px"
                       width={36}
                       height={36}
+                      className="rounded-full"
                     />
                   </button>
                 </div>
@@ -84,9 +110,12 @@ export const Footer = () => {
               <div className="w-[35px] h-[4px] bg-sunset rounded-t-2xl invisible"></div>
             </div>
           )}
+          {/*  */}
+          {/* Logout Button */}
           <button onClick={() => signOut()} className="text-[35px] flex">
             <FiLogOut />
           </button>
+          {/*  */}
         </div>
       </footer>
     </>
