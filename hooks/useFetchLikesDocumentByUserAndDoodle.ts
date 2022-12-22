22c557410a5ece385791fc15edfb27d9b682cffd
Peter Yoo <trustintofu@gmail.com>
2022-12-21 18:53:33 -0800

    feat: Creates custom hook that fetches likes document.

---
+++ hooks/useFetchLikesDocumentByUserAndDoodle.ts
@@ -0,0 +1,29 @@
+import { useQuery } from '@tanstack/react-query';
+
+const useFetchLikesDocumentByUserAndDoodle = (
+  doodleId: string,
+  userId: string
+) => {
+  const {
+    data: dataLikesDocumentByUserAndDoodle,
+    isLoading: isLoadingLikesDocumentByUserAndDoodle,
+    isError: isErrorLikesDocumentByUserAndDoodle,
+    refetch: refetchLikesDocumentByUserAndDoodle,
+  } = useQuery(['likesDocumentByUserAndDoodle'], async () => {
+    try {
+      const response = await fetch(`/api/doodles/${doodleId}/likes/${userId}`);
+      return response.json();
+    } catch (error) {
+      throw error;
+    }
+  });
+
+  return {
+    dataLikesDocumentByUserAndDoodle,
+    isLoadingLikesDocumentByUserAndDoodle,
+    isErrorLikesDocumentByUserAndDoodle,
+    refetchLikesDocumentByUserAndDoodle,
+  };
+};
+
+export default useFetchLikesDocumentByUserAndDoodle;
