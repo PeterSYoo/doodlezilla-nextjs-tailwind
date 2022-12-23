a40660d8daff3929e4a0760cbf399ee2ee86597d
Peter Yoo <trustintofu@gmail.com>
2022-12-23 00:55:40 -0800

    refactor: Moves data object to useMutation param.

--- hooks/useCreateNewLikesDocument.ts
+++ hooks/useCreateNewLikesDocument.ts
@@ -1,33 +1,32 @@
 import { useMutation } from '@tanstack/react-query';
 
-const useCreateNewLikesDocument = (
-  data: any,
-  doodleId: string,
-  userId: string
-) => {
+const useCreateNewLikesDocument = (doodleId: string, userId: string) => {
   const {
     mutate: mutateCreateNewLikesDocument,
     isLoading: isLoadingCreateNewLikesDocument,
-  } = useMutation(async () => {
-    try {
-      const Options = {
-        method: 'POST',
-        headers: { 'Content-Type': 'application/json' },
-        body: JSON.stringify({ data }),
-      };
-      const response = await fetch(
-        `/api/doodles/${doodleId}/likes/${userId}`,
-        Options
-      );
-      const json = await response.json();
+  } = useMutation(
+    async (data: any) => {
+      try {
+        const Options = {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({ data }),
+        };
+        const response = await fetch(
+          `/api/doodles/${doodleId}/likes/${userId}`,
+          Options
+        );
+        const json = await response.json();
 
-      if (json) {
-        return json;
+        if (json) {
+          return json;
+        }
+      } catch (error) {
+        return error;
       }
-    } catch (error) {
-      return error;
-    }
-  });
+    },
+    { retry: true }
+  );
 
   return {
     mutateCreateNewLikesDocument,
