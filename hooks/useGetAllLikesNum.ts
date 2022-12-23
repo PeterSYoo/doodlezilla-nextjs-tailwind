57c3af30cce9c712f8e54529969dc6db54faaccf
Peter Yoo <trustintofu@gmail.com>
2022-12-22 17:34:30 -0800

    feat: Creates custom hook to get all likes num related to doodle.

---
+++ hooks/useGetAllLikesNum.ts
@@ -0,0 +1,30 @@
+import { useQuery } from '@tanstack/react-query';
+
+const useGetAllLikesNum = (doodleId: string) => {
+  const {
+    data: dataGetAllLikesNum,
+    isLoading: isLoadingGetAllLikesNum,
+    isError: isErrorGetAllLikesNum,
+    refetch: refetchGetAllLikesNum,
+  } = useQuery(
+    ['allDoodles'],
+    async () => {
+      try {
+        const response = await fetch(`/api/doodles/${doodleId}/likes-num`);
+        return response.json();
+      } catch (error) {
+        throw error;
+      }
+    },
+    { refetchOnMount: 'always' }
+  );
+
+  return {
+    dataGetAllLikesNum,
+    isLoadingGetAllLikesNum,
+    isErrorGetAllLikesNum,
+    refetchGetAllLikesNum,
+  };
+};
+
+export default useGetAllLikesNum;
