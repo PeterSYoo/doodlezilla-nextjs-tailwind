a026a5143662a33b7e2002f72023bd42d8086263
Peter Yoo <trustintofu@gmail.com>
2022-12-23 22:40:37 -0800

    feat: Adds ability to filter fetched users using input values, and display the data in the jsx using map and filter.

--- components/search/SearchInput.components.tsx
+++ components/search/SearchInput.components.tsx
@@ -1,23 +1,105 @@
-import { useState } from 'react';
+import Image from 'next/image';
+import Link from 'next/link';
+import { ChangeEvent, Fragment, useEffect, useState } from 'react';
 import { MdSearch, MdClear } from 'react-icons/md';
+import useFetchAllUsers from '../../hooks/useFetchAllUsers';
+import { LoaderSpinner } from '../LoaderSpinner.components';
+
+type User = {
+  _id: string;
+  name: string;
+  image: string;
+  bio: string;
+  createdAt: string;
+  updatedAt: string;
+};
 
 export const SearchInput = () => {
-  const [hasValue, setHasValue] = useState('');
+  const [searchTerm, setSearchTerm] = useState<string>('');
+  const [filteredUsers, setFilteredUsers] = useState<User[]>([]);
+
+  const { allUsersData, allUsersIsLoading, allUsersIsError } =
+    useFetchAllUsers();
+
+  const handleSearchOnChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setSearchTerm(e.target.value);
+  };
+
+  useEffect(() => {
+    const filtered = allUsersData?.filter((user: User) => {
+      return user.name.toLowerCase().includes(searchTerm.toLowerCase());
+    });
+    setFilteredUsers(filtered);
+  }, [searchTerm]);
+
+  if (allUsersIsLoading) return <LoaderSpinner />;
+  if (allUsersIsError) return <>Error</>;
 
   return (
     <>
-      <form className="md:w-full bg-zinc-50 w-[188px] flex items-center gap-4 rounded-full border border-grayBorder px-4 h-[39px]">
-        {!hasValue && <MdSearch className="text-placeholder text-xl" />}
-        <input
-          placeholder="Search"
-          type="text"
-          onChange={(e) => setHasValue(e.target.value)}
-          className="bg-zinc-50 focus:outline-none w-full"
-        />
-        {hasValue && (
-          <MdClear className="text-xl text-placeholder cursor-pointer" />
+      <div
+        className={
+          searchTerm
+            ? 'md:w-full bg-zinc-50 w-[188px] flex items-center gap-4 rounded-full border border-blue-500 px-4 h-[39px]'
+            : 'md:w-full bg-zinc-50 w-[188px] flex items-center gap-4 rounded-full border border-grayBorder px-4 h-[39px]'
+        }
+      >
+        {!searchTerm && (
+          <MdSearch className="text-placeholder text-3xl lg:text-xl" />
         )}
-      </form>
+        {/* Username */}
+        <label className="w-full bg-zinc-50 rounded-full border border-transparent">
+          <input
+            placeholder="Search Users"
+            type="text"
+            value={searchTerm}
+            onChange={(e) => handleSearchOnChange(e)}
+            className="w-full bg-zinc-50 focus:outline-none"
+          />
+        </label>
+        {/*  */}
+        {searchTerm && (
+          <MdClear
+            onClick={() => setSearchTerm('')}
+            className="text-3xl lg:text-xl text-placeholder cursor-pointer"
+          />
+        )}
+      </div>
+      {searchTerm && (
+        <div className="w-[188px] md:w-[210px] lg:w-[400px] flex gap-4 rounded-md border border-grayBorder px-7 absolute bg-white mt-1 md:mt-[45px] py-5 backdrop-blur-sm bg-opacity-75 flex-col">
+          {filteredUsers
+            ?.slice()
+            .sort((a, b) => {
+              if (a.name < b.name) return -1;
+              if (a.name > b.name) return 1;
+              return 0;
+            })
+            .sort(() => Math.random() - 0.5)
+            .slice(0, 20)
+            .map((user) => (
+              <Fragment key={user._id}>
+                <div className="flex gap-3">
+                  <Link href={`/profile/${user.name}`}>
+                    <Image
+                      src={
+                        user.image
+                          ? user.image
+                          : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
+                      }
+                      height={25}
+                      width={25}
+                      alt="user avatar"
+                      className="rounded-full"
+                    />
+                  </Link>
+                  <Link href={`/profile/${user.name}`}>
+                    <span className="font-semibold">{user.name}</span>
+                  </Link>
+                </div>
+              </Fragment>
+            ))}
+        </div>
+      )}
     </>
   );
 };
