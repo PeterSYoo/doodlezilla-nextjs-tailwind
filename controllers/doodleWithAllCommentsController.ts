2d258a65ef9ea638d302b4012c5682e02813531c
Peter Yoo <trustintofu@gmail.com>
2022-12-25 16:32:41 -0800

    feat: Creates controller function to get 3 results per page with page queries from all doodles with comments, user, and likes.

--- controllers/doodleWithAllCommentsController.ts
+++ controllers/doodleWithAllCommentsController.ts
@@ -4,6 +4,11 @@ import Doodles from '../models/Doodles';
 import LikesNum from '../models/LikesNum';
 import Users from '../models/Users';
 
+interface UserAndComment {
+  user: { [key: string]: any };
+  comments: { [key: string]: any };
+}
+
 /* GET all Doodles With all Comments Referencing Each Individual Doodle Document. */
 export const getAllDoodlesWithAllComments = async (
   req: NextApiRequest,
@@ -83,7 +88,7 @@ export const getDoodleWithAllComments = async (
           return res.status(404).json({ error: 'Comments not found' });
         }
 
-        const usersAndComments: any = [];
+        const usersAndComments: UserAndComment[] = [];
 
         const combinedData = {
           doodle,
@@ -133,6 +138,80 @@ export const getUserDoodleWithAllCommentsAndLikesNum = async (
 
       res.status(200).json(combinedData);
     }
+  } catch (error) {
+    res.status(404).json({ error: 'Error while fetching users doodles' });
+  }
+};
+/*  */
+
+/* GET all Doodles With all Comments and Likes Num */
+export const getAllDoodleWithAllCommentsAndLikesNum = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const doodles = await Doodles.find();
+    if (!doodles) return res.status(404).json({ error: 'Doodles not Found' });
+
+    let combinedData = [];
+    for (let i = 0; i < doodles.length; i++) {
+      let comments = await Comments.find({ doodle: doodles[i]._id });
+      let likesNum = await LikesNum.find({ doodle: doodles[i]._id });
+      let user = await Users.findById(doodles[i].user);
+
+      combinedData.push({
+        doodle: doodles[i],
+        likesNum: likesNum,
+        comments: comments,
+        user: user,
+      });
+    }
+
+    res.status(200).json(combinedData);
+  } catch (error) {
+    res.status(404).json({ error: 'Error while fetching users doodles' });
+  }
+};
+/*  */
+
+/* GET Infinite Scroll Pagination all Doodles */
+export const getInfiniteQuriesAllDoodles = async (
+  req: NextApiRequest,
+  res: NextApiResponse
+) => {
+  try {
+    const perPage = 3; // Number of doodles to display per page
+    const page =
+      typeof req.query.page === 'string' ? parseInt(req.query.page) : 1;
+
+    // Calculate the skip value for pagination
+    const skip = (page - 1) * perPage;
+
+    const doodles = await Doodles.find()
+      .sort({ created_at: -1 })
+      .skip(skip)
+      .limit(perPage);
+    if (!doodles) return res.status(404).json({ error: 'Doodles not Found' });
+
+    let combinedData = [];
+    for (let i = 0; i < doodles.length; i++) {
+      let comments = await Comments.find({ doodle: doodles[i]._id });
+      let likesNum = await LikesNum.find({ doodle: doodles[i]._id });
+      let user = await Users.findById(doodles[i].user);
+
+      combinedData.push({
+        doodle: doodles[i],
+        likesNum: likesNum,
+        comments: comments,
+        user: user,
+      });
+    }
+
+    // Calculate the previous and next page numbers
+    const previousPage = page > 1 ? page - 1 : null;
+    const nextPage = combinedData.length === perPage ? page + 1 : null;
+
+    res.status(200).json({ combinedData, previousPage, nextPage });
   } catch (error) {
     res.status(404).json({ error: 'Error while fetching users doodles' });
   }
