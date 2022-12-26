b4d83b5b186fbd822ad0b906655d38ecf91f56e8
Peter Yoo <trustintofu@gmail.com>
2022-12-26 15:11:13 -0800

    feat: Refactors feed layout to masonry grid, adds dark classes, imports time difference functions.

--- pages/feed/index.tsx
+++ pages/feed/index.tsx
@@ -1,4 +1,4 @@
-import { Fragment, useEffect, useState } from 'react';
+import { Fragment, useEffect, useRef, useState } from 'react';
 import { GetServerSideProps } from 'next';
 import { unstable_getServerSession } from 'next-auth';
 import Image from 'next/image';
@@ -13,6 +13,14 @@ import { LoaderSpinnerInline } from '../../components/LoaderSpinnerInline.compon
 import { ProfileDoodleCardModal } from '../../components/profile/doodle/ProfileDoodleCardModal';
 import { AiFillHeart } from 'react-icons/ai';
 import { FaComment } from 'react-icons/fa';
+import {
+  getDayDifference,
+  getHourDifference,
+  getMinuteDifference,
+  getSecondsDifference,
+} from '../../utils/findTimeDifference';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
 
 type Session = {
   expires: string;
@@ -60,8 +68,13 @@ const FeedPage = ({ session }: FeedPageProps) => {
   const [tempUserId, setTempUserId] = useState<string>('');
   const [isHandleClick, setIsHandleClick] = useState<boolean>(false);
   const [tempUserFeed, setTempUserFeed] = useState<string>('');
+  const [pinSize, setPinSize] = useState<string>('');
+  const imgRef = useRef<any>(null);
 
   const { ref, inView } = useInView();
+  const router = useRouter();
+
+  const isFeedPage = router.asPath === '/feed';
 
   const {
     dataAllDoodlesWithCommentsAndLikesNum,
@@ -88,70 +101,6 @@ const FeedPage = ({ session }: FeedPageProps) => {
   const { userData, userIsLoading, userIsError, userRefetch } =
     useFetchUser(tempUserFeed);
 
-  const getDayDifference = (dateTime: string) => {
-    const currentTime = new Date().toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const specificDate = new Date(dateTime).toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const currentTimeInMilliseconds = new Date(currentTime).getTime();
-    const specificDateInMilliseconds = new Date(specificDate).getTime();
-    const timeDifference =
-      currentTimeInMilliseconds - specificDateInMilliseconds;
-    const dayDifference = Math.floor(timeDifference / 1000 / 60 / 60 / 24);
-
-    return dayDifference;
-  };
-
-  const getHourDifference = (dateTime: string) => {
-    const currentTime = new Date().toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const specificDate = new Date(dateTime).toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const currentTimeInMilliseconds = new Date(currentTime).getTime();
-    const specificDateInMilliseconds = new Date(specificDate).getTime();
-    const timeDifference =
-      currentTimeInMilliseconds - specificDateInMilliseconds;
-    const timeDifferenceInHours = Math.floor(timeDifference / 1000 / 60 / 60);
-
-    return timeDifferenceInHours;
-  };
-
-  const getMinuteDifference = (dateTime: string) => {
-    const currentTime = new Date().toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const specificDate = new Date(dateTime).toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const currentTimeInMilliseconds = new Date(currentTime).getTime();
-    const specificDateInMilliseconds = new Date(specificDate).getTime();
-    const timeDifference =
-      currentTimeInMilliseconds - specificDateInMilliseconds;
-    const timeDifferenceInMinutes = Math.floor(timeDifference / 1000 / 60);
-
-    return timeDifferenceInMinutes;
-  };
-
-  const getSecondsDifference = (dateTime: string) => {
-    const currentTime = new Date().toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const specificDate = new Date(dateTime).toLocaleString('en-US', {
-      timeZone: 'America/Los_Angeles',
-    });
-    const currentTimeInMilliseconds = new Date(currentTime).getTime();
-    const specificDateInMilliseconds = new Date(specificDate).getTime();
-    const timeDifference =
-      currentTimeInMilliseconds - specificDateInMilliseconds;
-    const timeDifferenceInSeconds = Math.floor(timeDifference / 1000);
-
-    return timeDifferenceInSeconds;
-  };
-
   const handleModalClickUser = (userId: string) => {
     setTempUserFeed(userId);
   };
@@ -170,6 +119,7 @@ const FeedPage = ({ session }: FeedPageProps) => {
 
   useEffect(() => {
     setTempUserId(session?.user?.id);
+    fetchNextPageInfiniteQueriesAllDoodles();
   }, []);
 
   useEffect(() => {
@@ -200,123 +150,135 @@ const FeedPage = ({ session }: FeedPageProps) => {
           mutateCreateNewLikesDocument={mutateCreateNewLikesDocument}
           userId={tempUserId}
           refetchInfiniteQueriesAllDoodles={refetchInfiniteQueriesAllDoodles}
+          isFeedPage={isFeedPage}
         />
       ) : null}
-      <div className="md:ml-[94px] md:mr-[159px] lg:ml-[213px] lg:mr-[258px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start">
-        <h1 className="hidden font-bold text-2xl md:flex justify-start md:w-[440px] md:mt-5">
+      <div className="md:ml-[94px] md:mr-[159px] lg:ml-[213px] lg:mr-[258px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start dark:bg-shadeDark">
+        <h1 className="hidden font-bold text-2xl md:flex justify-start dark:text-egg px-24 w-full">
           Feed
         </h1>
-        {dataInfiniteQueriesAllDoodles?.pages.map((page: Page, i: number) => (
-          <Fragment key={i}>
-            {page.combinedData?.map((doodle: Doodle) => (
-              <Fragment key={doodle.doodle._id}>
-                <div
-                  onClick={() => {
-                    handleModalClickUser(doodle.doodle.user);
-                    handleModalClick(doodle.doodle._id);
-                  }}
-                  className="w-[375px] h-[397px] md:w-[472px] md:h-[500px] flex justify-center items-center py-2 group flex-col mb-5"
-                >
-                  <Image
-                    src={doodle.doodle.image}
-                    width={472}
-                    height={600}
-                    alt="tree"
-                    className="rounded-[50px] group border border-grayBorder md:w-[472px] md:h-[500px] w-[375px] h-[397px] object-contain"
-                  />
-                  <div className="flex flex-col gap-0.5 items-center absolute mb-[240px] ml-[220px] md:mb-[340px] md:ml-[320px] bg-opacity-50 backdrop-blur-sm bg-white rounded-3xl px-2 py-3">
-                    <Image
-                      src={
-                        doodle.user.image
-                          ? doodle.user.image
-                          : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
-                      }
-                      width={43}
-                      height={43}
-                      alt="avatar feed"
-                      className="rounded-full"
-                    />
-                    <span className="font-semibold break-all w-7/8 text-center text-xs">
-                      {doodle.user.name}
-                    </span>
-                    <p className="text-xs mt-0.5">
-                      {getDayDifference(doodle.doodle.created_at) > 0 ? (
-                        <>{getDayDifference(doodle.doodle.created_at)}d ago</>
-                      ) : (
-                        <>
-                          {getHourDifference(doodle.doodle.created_at) > 0 ? (
-                            <>
-                              {getHourDifference(doodle.doodle.created_at)}h ago
-                            </>
-                          ) : (
-                            <>
-                              {getMinuteDifference(doodle.doodle.created_at) >
-                              0 ? (
-                                <>
-                                  {getMinuteDifference(
-                                    doodle.doodle.created_at
-                                  )}
-                                  m ago
-                                </>
-                              ) : (
-                                <>
-                                  {getSecondsDifference(
-                                    doodle.doodle.created_at
-                                  )}
-                                  s ago
-                                </>
-                              )}
-                            </>
-                          )}
-                        </>
-                      )}
-                    </p>
-                  </div>
-                  <div className="overlay group-hover:bg-black group-hover:bg-opacity-5 absolute cursor-pointer text-white w-[375px] h-[397px] md:h-[500px] md:w-[472px] rounded-[50px] group-hover:backdrop-blur-sm">
-                    <div className="overlay-text p-4 flex justify-center gap-5 items-center h-full w-full invisible group-hover:visible flex-col">
-                      <div className="flex items-center gap-10">
-                        <div className="flex items-center gap-2">
-                          <AiFillHeart />
-                          {doodle.likesNum.length}
-                        </div>
-                        <div className="flex items-center gap-2">
-                          <FaComment className="transform -scale-x-100" />
-                          {doodle.comments.length}
+        <div className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 2xl:columns-5 3xl:columns-6 px-5 md:px-20 lg:px-20">
+          {dataInfiniteQueriesAllDoodles?.pages.map((page: Page, i: number) => (
+            <Fragment key={i}>
+              {page.combinedData?.map((doodle: Doodle) => (
+                <Fragment key={doodle.doodle._id}>
+                  <div className="mb-20">
+                    <div
+                      onClick={() => {
+                        handleModalClickUser(doodle.doodle.user);
+                        handleModalClick(doodle.doodle._id);
+                      }}
+                      className="overlay-container rounded-3xl overflow-hidden relative group"
+                    >
+                      <Image
+                        src={doodle.doodle.image}
+                        alt="doodle card"
+                        width="0"
+                        height="0"
+                        sizes="100vw"
+                        className="rounded-3xl border border-grayBorder dark:border-transparent object-cover h-full w-full cursor-pointer"
+                      />
+                      <div className="overlay group-hover:bg-black group-hover:bg-opacity-30 group-hover:backdrop-blur-sm dark:group-hover:bg-white dark:group-hover:bg-opacity-30 dark:group-hover:backdrop-blur-sm absolute top-0 cursor-pointer text-white w-full h-full rounded-3xl">
+                        <div className="overlay-text p-4 flex justify-center gap-5 items-center h-full w-full invisible group-hover:visible flex-col">
+                          <div className="flex items-center gap-10 dark:text-midnight">
+                            <div className="flex items-center gap-2">
+                              <AiFillHeart />
+                              {doodle.likesNum.length}
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <FaComment className="transform -scale-x-100" />
+                              {doodle.comments.length}
+                            </div>
+                          </div>
                         </div>
                       </div>
                     </div>
+                    <div className="flex items-center absolute px-2 mt-2 justify-between gap-1">
+                      <Link
+                        href={`/profile/${doodle.user.name}`}
+                        className="group"
+                      >
+                        <div className="flex items-center gap-2">
+                          <Image
+                            src={
+                              doodle.user.image
+                                ? doodle.user.image
+                                : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
+                            }
+                            width={33}
+                            height={33}
+                            alt="avatar feed"
+                            className="rounded-full"
+                          />
+                          <span className="font-semibold break-all w-7/8 text-center text-xs dark:text-egg dark:group-hover:text-sunset group-hover:text-sunset cursor-pointer">
+                            {doodle.user.name}
+                          </span>
+                        </div>
+                      </Link>
+                      <p className="text-xs mt-0.5 dark:text-shadeText">
+                        -&nbsp;
+                        {getDayDifference(doodle.doodle.created_at) > 0 ? (
+                          <>{getDayDifference(doodle.doodle.created_at)}d ago</>
+                        ) : (
+                          <>
+                            {getHourDifference(doodle.doodle.created_at) > 0 ? (
+                              <>
+                                {getHourDifference(doodle.doodle.created_at)}h
+                                ago
+                              </>
+                            ) : (
+                              <>
+                                {getMinuteDifference(doodle.doodle.created_at) >
+                                0 ? (
+                                  <>
+                                    {getMinuteDifference(
+                                      doodle.doodle.created_at
+                                    )}
+                                    m ago
+                                  </>
+                                ) : (
+                                  <>
+                                    {getSecondsDifference(
+                                      doodle.doodle.created_at
+                                    )}
+                                    s ago
+                                  </>
+                                )}
+                              </>
+                            )}
+                          </>
+                        )}
+                      </p>
+                    </div>
                   </div>
-                </div>
-                {/*  */}
-              </Fragment>
-            ))}
-          </Fragment>
-        ))}
-        <div>
-          <button
-            ref={ref}
-            onClick={() => fetchNextPageInfiniteQueriesAllDoodles()}
-            disabled={
-              !hasNextPageInfiniteQueriesAllDoodles ||
-              isFetchingNextPageInfiniteQueriesAllDoodles
-            }
-            className="text-placeholder text-xs"
-          >
-            {isFetchingNextPageInfiniteQueriesAllDoodles ? (
-              <LoaderSpinnerInline />
-            ) : hasNextPageInfiniteQueriesAllDoodles ? (
-              'Load Newer'
-            ) : (
-              'Nothing more to load'
-            )}
-          </button>
+                </Fragment>
+              ))}
+            </Fragment>
+          ))}
         </div>
-        <div>
-          {isFetchingInfiniteQueriesAllDoodles &&
-          !isFetchingNextPageInfiniteQueriesAllDoodles ? (
+      </div>
+      <div className="flex flex-col justify-center mb-48 md:-ml-10 md:mb-20">
+        <button
+          ref={ref}
+          onClick={() => fetchNextPageInfiniteQueriesAllDoodles()}
+          disabled={
+            !hasNextPageInfiniteQueriesAllDoodles ||
+            isFetchingNextPageInfiniteQueriesAllDoodles
+          }
+          className="text-placeholder text-xs mb-5"
+        >
+          {isFetchingNextPageInfiniteQueriesAllDoodles ? (
             <LoaderSpinnerInline />
-          ) : null}
-        </div>
+          ) : hasNextPageInfiniteQueriesAllDoodles ? (
+            'Load Newer'
+          ) : (
+            'Nothing more to load'
+          )}
+        </button>
+        {isFetchingInfiniteQueriesAllDoodles &&
+        !isFetchingNextPageInfiniteQueriesAllDoodles ? (
+          <LoaderSpinnerInline />
+        ) : null}
       </div>
     </>
   );
