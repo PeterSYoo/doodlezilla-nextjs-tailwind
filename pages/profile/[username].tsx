e483949683c1283d7b893234247f024d35a8bca6
Peter Yoo <trustintofu@gmail.com>
2022-12-26 15:09:34 -0800

    feat: Refactors layout, implements sort by most recent created date.

--- pages/profile/[username].tsx
+++ pages/profile/[username].tsx
@@ -47,6 +47,22 @@ type SessionSSR = {
   expires: string;
 } | null;
 
+type DoodleSort = {
+  _id: string;
+  user: string;
+  image: string;
+  likes: number;
+  created_at: string;
+  updated_at: string;
+  __v: number;
+};
+
+type DoodleData = {
+  doodle: DoodleSort;
+  likesNum: number[];
+  comments: Comment[];
+};
+
 const UserIdPage = ({ session, username }: ProfileUserIdPageProps) => {
   const [isModal, setIsModal] = useState<boolean>(false);
   const [isDoodleModal, setIsDoodleModal] = useState<boolean>(false);
@@ -105,102 +121,75 @@ const UserIdPage = ({ session, username }: ProfileUserIdPageProps) => {
       <ProfileSlugUsersRightBar username={username} session={session} />
       <>
         {isModal ? <ProfileEditModal setIsModal={setIsModal} /> : null}
-        <div className="md:ml-[94px] md:mr-[159px] lg:ml-[213px] lg:mr-[258px] flex-grow flex flex-col items-center gap-5 mt-24 mb-32 md:justify-start">
-          {/* Profile Avatar & bio */}
-          <div className="md:hidden grid grid-cols-12 w-[375px] gap-3">
-            <div className="col-start-1 col-span-5 flex flex-col items-center gap-4 border border-grayBorder rounded-2xl py-4 px-2">
-              <div className="flex flex-col items-center gap-1">
-                <Image
-                  src={
-                    userData.image
-                      ? userData.image
-                      : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
-                  }
-                  width={75}
-                  height={75}
-                  alt="profile avatar mobile"
-                  className="rounded-full aspect-square"
-                />
-                <h1 className="font-bold break-all text-center">
-                  {userData.name}
-                </h1>
-                <p className="font-semibold text-xs">
-                  {userDoodlesWithAllCommentsAndLikesNumData.length}&nbsp;
-                  <span className="font-normal text-placeholder">DOODLES</span>
-                </p>
-              </div>
-              <p className="font-semibold text-xs text-placeholder">
-                {userData.location ? userData.location : null}
-              </p>
-              <button
-                onClick={() => setIsModal(true)}
-                className="border py-1 w-10/12 text-xs rounded-lg border-placeholder transition duration-100 ease-in-out hover:animate-button hover:bg-[length:400%_400%] bg-gradient-to-tr hover:from-[#F97E1C] hover:via-sunset hover:to-[#D055D3] hover:border-white hover:text-white font-semibold"
-              >
-                Edit
-              </button>
-            </div>
-            <div className="col-start-6 col-span-7 mt-4 flex flex-col gap-3">
-              <p className="text-xs">
-                {userData.biography ? (
-                  userData.biography
-                ) : (
-                  <>Please edit your profile to write your biography.</>
-                )}
-              </p>
-            </div>
-          </div>
-          <div className="border-b border-grayBorder w-[375px] md:hidden"></div>
-          {/*  */}
+        {isDoodleModal ? (
+          <ProfileDoodleCardModal
+            setIsDoodleModal={setIsDoodleModal}
+            userData={userData}
+            doodleId={tempDoodleId}
+            userDoodlesWithAllCommentsRefetch={
+              userDoodlesWithAllCommentsAndLikesNumRefetch
+            }
+            mutateCreateNewLikesDocument={mutateCreateNewLikesDocument}
+            userId={tempUserId}
+          />
+        ) : null}
+        <div className="md:ml-[94px] md:mr-[159px] lg:ml-[213px] lg:mr-[258px] flex-grow flex flex-col justify-center items-center gap-5 mt-24 mb-32 md:justify-start dark:bg-shadeDark">
           {/* Header Text */}
-          <h1 className="hidden font-bold text-2xl md:flex justify-start md:w-3/4 lg:w-[733px] md:mt-5">
+          <h1 className="hidden font-bold text-2xl md:flex justify-start dark:text-egg px-24 w-full">
             Doodles
           </h1>
-          {/*  */}
-          <div className="border-b border-grayBorder md:w-3/4 lg:w-[733px]"></div>
           {/* Doodles List */}
-          <div className="w-[375px] md:w-3/4 lg:w-[733px] grid grid-cols-3 gap-2 md:gap-4 lg:gap-5 px-3 md:px-0">
-            {isDoodleModal ? (
-              <ProfileDoodleCardModal
-                setIsDoodleModal={setIsDoodleModal}
-                userData={userData}
-                doodleId={tempDoodleId}
-                userDoodlesWithAllCommentsRefetch={
-                  userDoodlesWithAllCommentsAndLikesNumRefetch
-                }
-                mutateCreateNewLikesDocument={mutateCreateNewLikesDocument}
-                userId={tempUserId}
-              />
-            ) : null}
-            {userDoodlesWithAllCommentsAndLikesNumData.map((doodle: Doodle) => (
-              <Fragment key={doodle.doodle._id}>
-                <div
-                  onClick={() => {
-                    handleModalClick(doodle.doodle._id);
-                  }}
-                  className="overlay-container rounded-xl overflow-hidden relative group h-[200px]"
-                >
-                  <Image
-                    src={doodle.doodle.image}
-                    alt="doodle card"
-                    width={244}
-                    height={325}
-                    className="rounded-xl border border-grayBorder object-contain h-full"
-                  />
-                  <div className="overlay group-hover:bg-black group-hover:bg-opacity-5 absolute top-0 w-full h-full cursor-pointer text-white group-hover:backdrop-blur-sm">
-                    <div className="overlay-text p-4 flex justify-center gap-5 items-center h-full w-full invisible group-hover:visible">
-                      <div className="flex items-center gap-2">
-                        <AiFillHeart />
-                        {doodle.likesNum.length}
-                      </div>
-                      <div className="flex items-center gap-2">
-                        <FaComment className="transform -scale-x-100" />
-                        {doodle.comments.length}
+          <div className="columns-1 md:columns-2 lg:columns-3 xl:columns-4 2xl:columns-5 3xl:columns-6 px-5 md:px-20 lg:px-20">
+            <div>
+              {userDoodlesWithAllCommentsAndLikesNumData
+                .sort((a: DoodleData, b: DoodleData) => {
+                  // Get the doodle objects from the doodle data objects
+                  const doodleA = a.doodle;
+                  const doodleB = b.doodle;
+
+                  // Compare the created_at properties of the doodle objects
+                  if (doodleA.created_at < doodleB.created_at) {
+                    return 1;
+                  } else if (doodleA.created_at > doodleB.created_at) {
+                    return -1;
+                  } else {
+                    return 0;
+                  }
+                })
+                .map((doodle: Doodle) => (
+                  <Fragment key={doodle.doodle._id}>
+                    <div className="mb-5">
+                      <div
+                        onClick={() => {
+                          handleModalClick(doodle.doodle._id);
+                        }}
+                        className="overlay-container rounded-3xl overflow-hidden relative group h-full w-full"
+                      >
+                        <Image
+                          src={doodle.doodle.image}
+                          alt="doodle card"
+                          width="0"
+                          height="0"
+                          sizes="100vw"
+                          className="rounded-3xl border border-grayBorder dark:border-transparent object-cover h-full w-full cursor-pointer"
+                        />
+                        <div className="overlay group-hover:bg-black dark:group-hover:bg-white dark:group-hover:bg-opacity-30 group-hover:bg-opacity-30 absolute top-0 w-full h-full cursor-pointer text-white dark:text-black group-hover:backdrop-blur-sm dark:group-hover:backdrop-blur-sm">
+                          <div className="overlay-text p-4 flex justify-center gap-5 items-center h-full w-full invisible group-hover:visible">
+                            <div className="flex items-center gap-2">
+                              <AiFillHeart />
+                              {doodle.likesNum.length}
+                            </div>
+                            <div className="flex items-center gap-2">
+                              <FaComment className="transform -scale-x-100" />
+                              {doodle.comments.length}
+                            </div>
+                          </div>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                </div>
-              </Fragment>
-            ))}
+                  </Fragment>
+                ))}
+            </div>
           </div>
           {/*  */}
         </div>
