a0c5762737024cd6746a434066613d13454ca167
Peter Yoo <trustintofu@gmail.com>
2022-12-25 16:33:37 -0800

    feat: Creates hook for useInfiniteQuery.

---
+++ hooks/useInfiniteQueriesAllDoodles.ts
@@ -0,0 +1,42 @@
+import { useInfiniteQuery } from '@tanstack/react-query';
+
+const useInfiniteQueriesAllDoodles = () => {
+  const {
+    data: dataInfiniteQueriesAllDoodles,
+    isLoading: isLoadingInfiniteQueriesAllDoodles,
+    isError: isErrorInfiniteQueriesAllDoodles,
+    error: errorInfiniteQueriesAllDoodles,
+    hasNextPage: hasNextPageInfiniteQueriesAllDoodles,
+    fetchNextPage: fetchNextPageInfiniteQueriesAllDoodles,
+    isFetching: isFetchingInfiniteQueriesAllDoodles,
+    isFetchingNextPage: isFetchingNextPageInfiniteQueriesAllDoodles,
+  } = useInfiniteQuery(
+    ['infiniteQueriesAllDoodles'],
+    async ({ pageParam = 1 }) => {
+      try {
+        const response = await fetch(
+          `/api/all-doodles-with-comments-and-likes?page=${pageParam}`
+        );
+        return response.json();
+      } catch (error) {
+        throw error;
+      }
+    },
+    {
+      getNextPageParam: (lastPage, pages) => lastPage.nextPage,
+    }
+  );
+
+  return {
+    dataInfiniteQueriesAllDoodles,
+    isLoadingInfiniteQueriesAllDoodles,
+    isErrorInfiniteQueriesAllDoodles,
+    errorInfiniteQueriesAllDoodles,
+    hasNextPageInfiniteQueriesAllDoodles,
+    fetchNextPageInfiniteQueriesAllDoodles,
+    isFetchingInfiniteQueriesAllDoodles,
+    isFetchingNextPageInfiniteQueriesAllDoodles,
+  };
+};
+
+export default useInfiniteQueriesAllDoodles;
