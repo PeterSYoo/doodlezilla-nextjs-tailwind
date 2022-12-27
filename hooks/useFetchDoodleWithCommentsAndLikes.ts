ffe78dbb6c94cce93e076a5265591f2ae7617ea5
Peter Yoo <trustintofu@gmail.com>
2022-12-27 01:49:57 -0800

    feat: Creates custom hook for fetching single doodle with comments and likes.

---
+++ hooks/useFetchDoodleWithCommentsAndLikes.ts
@@ -0,0 +1,32 @@
+import { useQuery } from '@tanstack/react-query';
+
+const useFetchDoodleWithCommentsAndLikes = (doodleId: string) => {
+  const {
+    data: dataDoodleWithCommentsAndLikes,
+    isLoading: isLoadingDoodleWithCommentsAndLikes,
+    isError: isErrorDoodleWithCommentsAndLikes,
+    refetch: refetchDoodleWithCommentsAndLikes,
+  } = useQuery(
+    ['doodleWithCommentsAndLikes', doodleId],
+    async () => {
+      try {
+        const response = await fetch(
+          `/api/doodle-with-comments-and-likes/${doodleId}`
+        );
+        return response.json();
+      } catch (error) {
+        throw error;
+      }
+    },
+    { refetchOnMount: 'always' }
+  );
+
+  return {
+    dataDoodleWithCommentsAndLikes,
+    isLoadingDoodleWithCommentsAndLikes,
+    isErrorDoodleWithCommentsAndLikes,
+    refetchDoodleWithCommentsAndLikes,
+  };
+};
+
+export default useFetchDoodleWithCommentsAndLikes;
