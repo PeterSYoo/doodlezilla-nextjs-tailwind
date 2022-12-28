17f62956e42a3dee04ff01850e7aef8104647dab
Peter Yoo <trustintofu@gmail.com>
2022-12-27 20:16:26 -0800

    feat: Adds text hover classes, changed height.

--- components/Footer.components.tsx
+++ components/Footer.components.tsx
@@ -26,11 +26,11 @@ export const Footer = () => {
 
   return (
     <>
-      <footer className="z-40 fixed bottom-0 flex w-full h-[75px] bg-white dark:bg-midnight border-t border-grayBorder dark:border-transparent text-4xl px-9 pt-3 text-placeholder backdrop-blur-sm bg-opacity-75 dark:backdrop-blur-sm dark:bg-opacity-75">
+      <footer className="z-40 fixed bottom-0 flex w-full h-[50px] bg-white dark:bg-midnight border-t border-grayBorder dark:border-transparent text-xl px-9 pt-3 text-placeholder backdrop-blur-sm bg-opacity-75 dark:bg-opacity-90 dark:backdrop-blur-sm dark:bg-opacity-75">
         <div className="flex justify-between w-full max-w-[375px] mx-auto">
           {/* Feed Button */}
           {isFeedPage ? (
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-between items-center">
               <Link href="/feed">
                 <button>
                   <AiFillHome className="text-sunset" />
@@ -39,10 +39,10 @@ export const Footer = () => {
               <div className="w-[35px] h-[4px] bg-sunset rounded-t-2xl"></div>
             </div>
           ) : (
-            <div className="flex flex-col justify-between dark:text-egg">
+            <div className="flex flex-col justify-between dark:text-egg items-center">
               <Link href="/feed">
                 <button>
-                  <AiFillHome />
+                  <AiFillHome className="hover:text-sunset dark:hover:text-sunset" />
                 </button>
               </Link>
               <div className="w-[35px] h-[4px] bg-sunset rounded-t-2xl invisible"></div>
@@ -51,7 +51,7 @@ export const Footer = () => {
           {/*  */}
           {/* Create Button */}
           {isCreatePage ? (
-            <div className="flex flex-col justify-between">
+            <div className="flex flex-col justify-between items-center">
               <Link href="/create">
                 <MdCreate className="text-sunset" />
               </Link>
@@ -59,7 +59,7 @@ export const Footer = () => {
             </div>
           ) : (
             <Link href="/create">
-              <MdCreate className="dark:text-egg" />
+              <MdCreate className="dark:text-egg hover:text-sunset dark:hover:text-sunset" />
             </Link>
           )}
           {/*  */}
@@ -76,8 +76,8 @@ export const Footer = () => {
                           : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
                       }
                       alt="avatar 36px"
-                      width={36}
-                      height={36}
+                      width={20}
+                      height={20}
                       className="rounded-full"
                     />
                   </button>
@@ -88,7 +88,7 @@ export const Footer = () => {
           ) : (
             <div className="-mt-1 flex flex-col justify-between items-center">
               <Link href="/profile">
-                <div className="animate-border rounded-full from-transparent via-transparent to-transparent bg-[length:400%_400%] p-0.5 bg-transparent bg-gradient-to-tr duration-300">
+                <div className="animate-border rounded-full from-transparent via-transparent to-transparent bg-[length:400%_400%] p-0.5 bg-transparent bg-gradient-to-tr duration-300 dark:hover:from-[#D055D3] dark:hover:via-sunset dark:hover:to-[#F97E1C]">
                   <button className="bg-transparent rounded-full p-0.5 flex justify-center items-center">
                     <Image
                       src={
@@ -97,8 +97,8 @@ export const Footer = () => {
                           : 'https://res.cloudinary.com/dryh1nvhk/image/upload/v1671393782/nudoodle/assets/user-avatar_th6utq.png'
                       }
                       alt="avatar 36px"
-                      width={36}
-                      height={36}
+                      width={20}
+                      height={20}
                       className="rounded-full"
                     />
                   </button>
@@ -109,8 +109,8 @@ export const Footer = () => {
           )}
           {/*  */}
           {/* Logout Button */}
-          <button onClick={() => signOut()} className="text-[35px] flex">
-            <FiLogOut className="dark:text-egg" />
+          <button onClick={() => signOut()} className="flex">
+            <FiLogOut className="dark:text-egg hover:text-sunset dark:hover:text-sunset" />
           </button>
           {/*  */}
         </div>
